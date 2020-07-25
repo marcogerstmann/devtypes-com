7e9583d86addb599604d1d7765623d7e3ec48e5e
Marco Gerstmann <m.gerstmann@mailbox.org>
2020-07-25 19:09:41 +0200

    refactor to side widget

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-import Img from "gatsby-image"
 import { RiArrowRightSLine } from "react-icons/ri"
 
 import Layout from "../components/layout"
@@ -14,17 +13,6 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        tagline
-        featuredImage {
-          childImageSharp {
-            fluid(maxWidth: 480, maxHeight: 380, quality: 80, srcSetBreakpoints: [960, 1440]) {
-              ...GatsbyImageSharpFluid
-            }
-            sizes {
-              src
-            }
-          }
-        }
         cta {
           ctaText
           ctaLink
@@ -37,28 +25,17 @@ export const pageQuery = graphql`
 const HomePage = ({ data }) => {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
-  const Image = frontmatter.featuredImage ? frontmatter.featuredImage.childImageSharp.fluid : ""
 	return (
 		<Layout>
-      <SEO/>
-      <div className="home-banner grids col-1 sm-2">
-        <div>
-          <h1 className="title">{frontmatter.title}</h1>
-          <p className="tagline">{frontmatter.tagline}</p>
-          <div className="description" dangerouslySetInnerHTML={{__html: html}}/>
-          <Link to={frontmatter.cta.ctaLink} className="button">{frontmatter.cta.ctaText}<span className="icon -right"><RiArrowRightSLine/></span></Link>
-        </div>
-        <div>
-          {Image ? (
-            <Img 
-              fluid={Image} 
-              alt={frontmatter.title + ' - Featured image'}
-              className="featured-image"
-            />
-          ) : ""}
-        </div>
-      </div>
-      <BlogListHome/>
+		  <SEO/>
+		  <div className="home-banner grids col-1 sm-2">
+			<div>
+			  <h1 className="title">{frontmatter.title}</h1>
+			  <div className="description" dangerouslySetInnerHTML={{__html: html}}/>
+			  <Link to={frontmatter.cta.ctaLink} className="button">{frontmatter.cta.ctaText}<span className="icon -right"><RiArrowRightSLine/></span></Link>
+			</div>
+		  </div>
+		  <BlogListHome/>
 		</Layout>
 	)
 }
